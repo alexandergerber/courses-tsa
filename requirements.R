383d47b4859b70d0f23087a25d84944d1398e044
alexandergerber <AlexanderGerber@web.de>
2019-02-20 14:53:23 +0100

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -3,7 +3,7 @@ library(remotes)
 
 install_version("ggplot2", "2.2.1")
 install_version("dplyr", "0.7.4")
-install_version("quantmod", "0.4.13") 
+install_version("quantmod", "0.4-13") 
 ##### Install packages in development from GitHub #####
 
 # install_github("nicholasehamilton/ggtern")
