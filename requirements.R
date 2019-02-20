202ab14c54608d358e55acfb00bc867506a03c07
alexandergerber <AlexanderGerber@web.de>
2019-02-20 14:41:37 +0100

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -2,10 +2,8 @@ library(remotes)
 ##### Install specific package versions from CRAN #####
 
 install_version("ggplot2", "2.2.1")
-# install_version("dplyr", "0.7.4")
-# install_version("tidyr", "0.8.0")
-# install_version("purrr", "0.2.4")
-
+install_version("dplyr", "0.7.4")
+install_version("quantmod", "0.4.13") 
 ##### Install packages in development from GitHub #####
 
 # install_github("nicholasehamilton/ggtern")
